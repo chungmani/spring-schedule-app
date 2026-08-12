9517ead54820bccef1a017565dc24e974dfc4a43
chungmani <cndaks19@naver.com>
2026-08-13 00:47:14 +0900

    lv1. 일정 조회 (단건 조회, 전체 조회) 기능 구현

--- src/main/java/com/example/springscheduleapp/controller/ScheduleController.java
+++ src/main/java/com/example/springscheduleapp/controller/ScheduleController.java
@@ -2,14 +2,14 @@ package com.example.springscheduleapp.controller;
 
 import com.example.springscheduleapp.dto.CreateScheduleRequest;
 import com.example.springscheduleapp.dto.CreateScheduleResponse;
+import com.example.springscheduleapp.dto.GetScheduleResponse;
 import com.example.springscheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +21,17 @@ public class ScheduleController {
     @PostMapping
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.create(request));
+    }
+
+    // 전체 일정 조회
+    @GetMapping
+    public ResponseEntity<List<GetScheduleResponse>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getAll());
+    }
+
+    // 단건 조회
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<GetScheduleResponse> getOne(@PathVariable Long scheduleId) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getOne(scheduleId));
     }
 }
--- src/main/java/com/example/springscheduleapp/service/ScheduleService.java
+++ src/main/java/com/example/springscheduleapp/service/ScheduleService.java
@@ -2,11 +2,14 @@ package com.example.springscheduleapp.service;
 
 import com.example.springscheduleapp.dto.CreateScheduleRequest;
 import com.example.springscheduleapp.dto.CreateScheduleResponse;
+import com.example.springscheduleapp.dto.GetScheduleResponse;
 import com.example.springscheduleapp.entity.Schedule;
 import com.example.springscheduleapp.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,28 @@ public class ScheduleService {
                 savedSchedule.getContent(), savedSchedule.getAuthor(),
                 savedSchedule.getCreatedAt(), savedSchedule.getModifiedAt()
         );
+    }
+
+    // 전체 조회 메서드
+    @Transactional(readOnly = true)
+    public List<GetScheduleResponse> getAll() {
+        List<Schedule> schedules = scheduleRepository.findAll();
+        return schedules.stream()
+                .map(schedule -> new GetScheduleResponse(
+                        schedule.getId(), schedule.getTitle(), schedule.getContent(),
+                        schedule.getAuthor(), schedule.getCreatedAt(), schedule.getModifiedAt()
+                ))
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GetScheduleResponse getOne(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 스케줄입니다.")
+        );
+
+        return new GetScheduleResponse(schedule.getId(), schedule.getTitle(),
+                schedule.getContent(), schedule.getAuthor(),
+                schedule.getCreatedAt(), schedule.getModifiedAt());
     }
 }

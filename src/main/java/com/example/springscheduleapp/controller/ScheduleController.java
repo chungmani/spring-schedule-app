b0dfcaf0ca76bbb8f975011acdf11220f02ae849
chungmani <cndaks19@naver.com>
2026-08-12 23:31:55 +0900

    lv1. 일정 생성 기능 구현

--- src/main/java/com/example/springscheduleapp/controller/ScheduleController.java
+++ src/main/java/com/example/springscheduleapp/controller/ScheduleController.java
@@ -1,12 +1,25 @@
 package com.example.springscheduleapp.controller;
 
+import com.example.springscheduleapp.dto.CreateScheduleRequest;
+import com.example.springscheduleapp.dto.CreateScheduleResponse;
 import com.example.springscheduleapp.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/schedules")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.create(request));
+    }
 }

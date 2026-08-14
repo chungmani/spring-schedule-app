2dcd6cc1d74689863b0fafa4169dce4795d2475a
chungmani <cndaks19@naver.com>
2026-08-14 15:58:44 +0900

    lv5. 댓글 생성 기능 구현 (하나의 스케줄당 10개까지만 가능)

--- src/main/java/com/example/springscheduleapp/comment/repository/CommentRepository.java
+++ src/main/java/com/example/springscheduleapp/comment/repository/CommentRepository.java
@@ -2,6 +2,10 @@ package com.example.springscheduleapp.comment.repository;
 
 import com.example.springscheduleapp.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("SELECT COUNT(c.schedule) FROM Comment c WHERE c.schedule.id = :scheduleId")
+    Integer findByCommentCount(@Param("scheduleId") Long scheduleId);
 }
--- src/main/java/com/example/springscheduleapp/comment/service/CommentService.java
+++ src/main/java/com/example/springscheduleapp/comment/service/CommentService.java
@@ -4,6 +4,7 @@ import com.example.springscheduleapp.comment.dto.CreateCommentRequest;
 import com.example.springscheduleapp.comment.dto.CreateCommentResponse;
 import com.example.springscheduleapp.comment.entity.Comment;
 import com.example.springscheduleapp.comment.repository.CommentRepository;
+import com.example.springscheduleapp.schedule.entity.Schedule;
 import com.example.springscheduleapp.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,20 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final ScheduleRepository scheduleRepository;
 
+    // 댓글 생성 (1스케줄당 10개까지만 가능)
     public CreateCommentResponse create(Long scheduleId, CreateCommentRequest request) {
-
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 스케줄입니다.")
+        );
+        int count = commentRepository.findByCommentCount(scheduleId);
+        if (count >= 10) {
+            throw new IllegalArgumentException("댓글은 10개까지만 생성가능합니다.");
+        }
+        Comment comment = new Comment(request.getContent(), request.getName(), request.getPassword(), schedule);
+        Comment savedComment = commentRepository.save(comment);
+        return new CreateCommentResponse(savedComment.getId(),
+                savedComment.getContent(), savedComment.getName(),
+                savedComment.getCreatedAt(), savedComment.getModifiedAt()
+        );
     }
 }

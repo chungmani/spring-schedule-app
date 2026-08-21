3b511758799845cd6c726cf2761cc720a5b8a252
chungmani <cndaks19@naver.com>
2026-08-22 00:16:54 +0900

    refactor / 생성일, 수정일 추가

--- src/main/java/com/example/springscheduleapp/user/dto/CreateUserResponse.java
+++ src/main/java/com/example/springscheduleapp/user/dto/CreateUserResponse.java
@@ -2,16 +2,22 @@ package com.example.springscheduleapp.user.dto;
 
 import com.example.springscheduleapp.user.entity.User;
 
+import java.time.LocalDateTime;
+
 public record CreateUserResponse(
         Long id,
         String name,
-        String email
+        String email,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
 ) {
     public static CreateUserResponse from(User user) {
         return new CreateUserResponse(
                 user.getId(),
                 user.getName(),
-                user.getEmail()
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
         );
     }
 }

c67faef26207f03e41e9f36145542811323fd547
chungmani <cndaks19@naver.com>
2026-08-22 20:28:25 +0900

    refactor / develop Lv5. valid 예외처리

--- src/main/java/com/example/springscheduleapp/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springscheduleapp/common/exception/GlobalExceptionHandler.java
@@ -3,13 +3,18 @@ package com.example.springscheduleapp.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 없는 스케줄이거나, 다른 아이디 스케줄일때
     @ExceptionHandler(ScheduleNotFoundException.class)
     public ResponseEntity<String> handleScheduleNotFound(ScheduleNotFoundException e) {
 
@@ -17,9 +22,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    // 로그인이 필요할때
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<String> handleUnauthorized(UnauthorizedException e) {
         log.error("[ERROR] Unauthorized 발생", e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    // validate에서 걸려졌을때, 동시에 여러 검증이 실패할수 있으므로 List로 반환
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidation(MethodArgumentNotValidException e) {
+        log.error("[ERROR] 검증 실패", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getBindingResult().getFieldErrors().stream().
+                                map(FieldError::getDefaultMessage).toList());
     }
 }
